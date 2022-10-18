603c84d8c218fbeb0d1e6649c3d246f292a6b2b3
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-10-18 23:09:42 +0100

    Added hillshade vectorization code to panama_hillshade.py

--- new_caledonia_maps/panama_hillshade.py
+++ new_caledonia_maps/panama_hillshade.py
@@ -1,3 +1,4 @@
+import subprocess
 from pathlib import Path
 
 from map_engraver.canvas.canvas_bbox import CanvasBbox
@@ -11,34 +12,80 @@ from map_engraver.data.geotiff.canvas_transform import \
     transform_geotiff_to_crs_within_canvas
 from pyproj import CRS
 
-canvas_width = Cu.from_px(720)
-canvas_height = Cu.from_px(500)
-canvas_bbox = CanvasBbox(
-    CanvasCoordinate.origin(),
-    canvas_width,
-    canvas_height
-)
-canvas_rect = rect(canvas_bbox)
-crs = CRS.from_proj4('+proj=utm +zone=17')
-wgs84_crs = CRS.from_epsg(4326)
-builder = GeoCanvasTransformersBuilder()
-builder.set_scale_and_origin_from_coordinates_and_crs(
-    crs,
-    GeoCoordinate(9, -83.5, wgs84_crs),
-    GeoCoordinate(9, -76.5, wgs84_crs),
-    CanvasCoordinate.from_px(0, canvas_height.px / 2),
-    CanvasCoordinate.from_px(canvas_width.px, canvas_height.px / 2)
-)
-builder.set_data_crs(wgs84_crs)
 
-data_path = Path(__file__).parent.parent.joinpath('data')
-input_file = data_path.joinpath('ne_10m_shaded_relief/SR_HR.tif')
-output_file = data_path.joinpath('panama_shaded_relief.tif')
+def docker_run(command: str) -> int:
+    return subprocess.call(
+        'docker run --rm -it '
+        '-v `pwd`:/root/mydata/ '
+        '-w /root/mydata/ '
+        'new-caledonia-maps-potrace '
+        + command,
+        shell=True,
+        cwd=Path(__file__).parent.parent.as_posix()
+    )
 
-transform_geotiff_to_crs_within_canvas(
-    input_file,
-    canvas_rect,
-    builder,
-    output_file
-)
 
+data_path = Path(__file__).parent.parent
+full_tif_filename = 'data/ne_10m_shaded_relief/SR_HR.tif'
+panama_tif_filename = 'data/panama_shaded_relief.tif'
+panama_pnm_filename = 'data/panama_shaded_relief.pnm'
+panama_svg_light_filename = 'data/panama_shaded_relief_light_%.2f.svg'
+panama_svg_dark_filename = 'data/panama_shaded_relief_dark_%.2f.svg'
+full_file_path = data_path.joinpath(full_tif_filename)
+panama_tif_path = data_path.joinpath(panama_tif_filename)
+panama_pnm_path = data_path.joinpath(panama_pnm_filename)
+
+if not panama_tif_path.exists():
+    canvas_width = Cu.from_px(720)
+    canvas_height = Cu.from_px(500)
+    canvas_bbox = CanvasBbox(
+        CanvasCoordinate.origin(),
+        canvas_width,
+        canvas_height
+    )
+    canvas_rect = rect(canvas_bbox)
+    crs = CRS.from_proj4('+proj=utm +zone=17')
+    wgs84_crs = CRS.from_epsg(4326)
+    builder = GeoCanvasTransformersBuilder()
+    builder.set_scale_and_origin_from_coordinates_and_crs(
+        crs,
+        GeoCoordinate(9, -83.5, wgs84_crs),
+        GeoCoordinate(9, -76.5, wgs84_crs),
+        CanvasCoordinate.from_px(0, canvas_height.px / 2),
+        CanvasCoordinate.from_px(canvas_width.px, canvas_height.px / 2)
+    )
+    builder.set_data_crs(wgs84_crs)
+
+    transform_geotiff_to_crs_within_canvas(
+        full_file_path,
+        canvas_rect,
+        builder,
+        panama_tif_path
+    )
+
+if not panama_pnm_path.exists():
+    docker_run(
+        'tifftopnm %s > %s' % (panama_tif_filename, panama_pnm_filename)
+    )
+
+threshold_midpoint = 206/255
+
+for threshold_delta in [0.05, 0.10, 0.15]:
+    threshold = threshold_midpoint + threshold_delta
+    docker_run(
+        'potrace %s -o %s -b svg -k %f -i' % (
+            panama_pnm_filename,
+            panama_svg_light_filename % threshold,
+            threshold
+        )
+    )
+
+for threshold_delta in [0.05, 0.10, 0.15, 0.25, 0.40]:
+    threshold = threshold_midpoint - threshold_delta
+    docker_run(
+        'potrace %s -o %s -b svg -k %f' % (
+            panama_pnm_filename,
+            panama_svg_dark_filename % threshold,
+            threshold
+        )
+    )
